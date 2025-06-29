330375697a4d8b3cc90d061f5e5b91e9daa5e498
sibom007 <vaispark400@gmail.com>
2025-06-29 12:45:13 +0600

    03.Add trpc with Tanstack query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { TextEffect } from "@/components/motion-primitives/text-effect";
-
-
 export default function Home() {
-  return (
-    <div className="text-3xl text-center font-bold text-orange-500 flex items-center justify-center h-40">
-      <TextEffect per="word" as="h1" preset="slide">
-        Hello welcome to Project
-      </TextEffect>
-    </div>
-  );
+  return <div>Hello</div>;
 }
